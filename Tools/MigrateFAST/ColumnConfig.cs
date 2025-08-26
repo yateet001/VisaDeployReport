a717e3248e86c7fa662d5039c8ac017d0c2dced3
Hitesh Mehata MAQ Software <hiteshm@maqsoftware.com>
2025-08-26 10:23:58 +0000

    Deleted ColumnConfig.cs

--- Tools/MigrateFAST/ColumnConfig.cs
+++
@@ -1,9 +0,0 @@
-namespace ExcelToTMDL.Models
-{
-    public class ColumnConfig
-    {
-        public string ColumnName { get; set; } = string.Empty;
-        public string DataType { get; set; } = string.Empty;
-        public string SourceColumn { get; set; } = string.Empty;
-    }
-}
