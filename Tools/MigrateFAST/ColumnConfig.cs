631a4fb1a91082f40d7482c8ffe67a5844bfbd69
Hitesh Mehata MAQ Software <hiteshm@maqsoftware.com>
2025-08-26 10:04:56 +0000

    Added 2 files to /Tools/MigrateFAST/Models

---
+++ Tools/MigrateFAST/ColumnConfig.cs
@@ -0,0 +1,9 @@
+namespace ExcelToTMDL.Models
+{
+    public class ColumnConfig
+    {
+        public string ColumnName { get; set; } = string.Empty;
+        public string DataType { get; set; } = string.Empty;
+        public string SourceColumn { get; set; } = string.Empty;
+    }
+}
